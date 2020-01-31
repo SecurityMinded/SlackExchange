58878bcdbe5ee67d8c7fc7a6702cb8cf74e72407
davide.guardini <davide.guardini@digitronica.it>
2020-01-31 12:33:58 +0100

    Initial Commit

---
+++ SlackExchange.API/Data/DataContext.cs
@@ -0,0 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using SlackExchange.API.Models;
+
+namespace SlackExchange.API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Value> Values { get; set; }
+    }
+}
