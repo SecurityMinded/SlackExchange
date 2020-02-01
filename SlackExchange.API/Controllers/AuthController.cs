53abcaf93a992181352046bda056d6c5ac0b5a80
davide.guardini <davide.guardini@digitronica.it>
2020-02-01 21:29:01 +0100

    Added TODOs

--- SlackExchange.API/Controllers/AuthController.cs
+++ SlackExchange.API/Controllers/AuthController.cs
@@ -50,6 +50,7 @@ namespace SlackExchange.API.Controllers
         [HttpPost("login")]
         public async Task<IActionResult> Login(UserForLoginDto userForLoginDto)
         {
+            // Aggiungere validazione username null
             var userFromRepo = await _repo.Login(userForLoginDto.Username.ToLower(), userForLoginDto.Password);
 
             if (userFromRepo == null)
