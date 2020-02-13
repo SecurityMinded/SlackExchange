4d7076aa0ecdc4be2858f1582ea863ee530129c6
davide.guardini <davide.guardini@digitronica.it>
2020-02-13 17:54:29 +0100

    Added components routing.

---
+++ SlackExchangeApp-SPA/src/app/routes.ts
@@ -0,0 +1,21 @@
+import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { UserListComponent } from './user-list/user-list.component';
+import { ListsComponent } from './lists/lists.component';
+import { MessagesComponent } from './messages/messages.component';
+import { AuthGuard } from './_guards/auth.guard';
+
+export const appRoutes: Routes = [
+    { path: '', component: HomeComponent },
+    {
+        path: '',
+        runGuardsAndResolvers: 'always',
+        canActivate: [AuthGuard],
+        children: [
+            { path: 'users', component: UserListComponent },
+            { path: 'messages', component: MessagesComponent },
+            { path: 'lists', component: ListsComponent },
+        ]
+    },
+    { path: '**', redirectTo: '', pathMatch: 'full' },
+];
